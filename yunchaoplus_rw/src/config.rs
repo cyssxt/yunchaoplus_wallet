f24e645b35d79128eb321a1e7b183b40e5dade61
lightsing <light.tsing@gmail.com>
2021-02-13 13:20:55 +0800

    add deadpool

---
+++ yunchaoplus_rw/src/config.rs
@@ -0,0 +1,18 @@
+use deadpool_postgres::{Manager, Pool};
+use dotenv::dotenv;
+use serde::Deserialize;
+use tokio::stream::StreamExt;
+use std::convert::TryInto;
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub pg: deadpool_postgres::Config
+}
+
+impl Config {
+    pub fn from_env() -> Result<Self, config::ConfigError> {
+        let mut cfg = config::Config::new();
+        cfg.merge(config::Environment::new().separator("__"))?;
+        cfg.try_into()
+    }
+}
--- yunchaoplus_rw/src/main.rs
+++ yunchaoplus_rw/src/main.rs
@@ -1,8 +1,14 @@
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 
-use actix_web::{App, HttpServer};
+use dotenv::dotenv;
 use actix_web::middleware::Logger;
+use actix_web::{App, HttpServer};
+use tokio_postgres::NoTls;
 
+use crate::config::Config;
+
+mod config;
 mod model;
 mod recharge;
 mod withdraw;
@@ -10,9 +16,14 @@ mod withdraw;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     pretty_env_logger::init();
+    dotenv().ok();
 
-    HttpServer::new(|| {
+    let cfg = Config::from_env().unwrap();
+    let pool = cfg.pg.create_pool(NoTls).unwrap();
+
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .wrap(Logger::default())
             .service(recharge::create_recharge)
             .service(recharge::get_recharge)
@@ -22,7 +33,7 @@ async fn main() -> std::io::Result<()> {
             .service(withdraw::update_withdraw)
             .service(withdraw::get_withdraw_list)
     })
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
